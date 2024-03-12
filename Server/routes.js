66d5c25191c670793cf87202cbcbefd092f32355
Ayushtiwari101 <ayush.tiwari@kalvium.community>
2024-03-12 16:41:42 +0530

    made changes

--- Server/routes.js
+++ Server/routes.js
@@ -1,14 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const {userModel} = require('./Schema')
-const cors = require('cors');
-
-const corsOptions ={
-    origin:'https://client-oat9qj8aj-ayush-tiwaris-projects-918925cc.vercel.app/', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-router.use(cors(corsOptions));
 
 
 router.use(express.json())
@@ -39,6 +31,7 @@ router.delete('/delete',(req,res)=>{
 
 router.post('/new', async (req, res) => {
     try {
+        console.log(req.body)
         const newData = await userModel.create(req.body);
         console.log(newData)
         res.send(newData);
