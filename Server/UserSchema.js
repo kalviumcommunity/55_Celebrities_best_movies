9441a3303627188142b336967b5a0f73ed350559
Ayushtiwari101 <ayush.tiwari@kalvium.community>
2024-03-21 09:37:59 +0530

    made changes in backend

--- Server/UserSchema.js
+++ Server/UserSchema.js
@@ -5,5 +5,5 @@ const userSchema = mongoose.Schema({
     password: String
     
 });
-const userModel = mongoose.model("User", userSchema);
-module.exports = {userModel};
+const Model = mongoose.model("users", userSchema);
+module.exports = {Model};
